d75b3fb00a8c33c03791419e77a44a4c0609fc21
Paulo <pauloitalo@hotmail.com.br>
2018-01-27 19:33:59 -0300

    requirements.txt

--- djangocommerce/settings.py
+++ djangocommerce/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'djangocommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = { }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
